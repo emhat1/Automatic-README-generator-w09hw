fa64b837d88977290052a3f1f82a36238cd9e846
Erin H <e.hatherell@gmail.com>
2022-01-30 18:20:15 +1100

    Minor code cleanup

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const generateMarkdown = require(".generateMarkdown.js");
 
 
-// TODO: Create an array of questions for user input
+// Questions for user input to generate the file
 const questions = [
         {
             type: 'input',
@@ -52,7 +52,7 @@ const questions = [
         },
 ];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
     console.log(fileName);
     console.log(data);
@@ -62,7 +62,7 @@ function writeToFile(fileName, data) {
     })
 }
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
   inquirer.prompt(questions).then((answers) =>
     writeToFile("README.md", generateMarkdown(answers))
