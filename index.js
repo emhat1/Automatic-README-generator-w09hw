eb3974b6337de32cb838296242de20d9c4a5d658
Erin H <e.hatherell@gmail.com>
2022-01-30 13:17:24 +1100

    Package updates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // Call up required packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
