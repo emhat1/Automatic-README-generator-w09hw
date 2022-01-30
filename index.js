18c11de1a8532f787b32ee0a09f321a1d5240883
Erin H <e.hatherell@gmail.com>
2022-01-30 13:59:26 +1100

    Savepoint prior to having a break

--- index.js
+++ index.js
@@ -11,25 +11,21 @@ const questions = [
             type: 'input',
             name: 'projectTitle',
             message: 'What is the name of your project?',
-            default: 'New project'
         },
         {
             type: 'input',
             name: 'projectDescription',
             message: 'Please describe the purpose of the project',
-            default: 'To make something easier'
         },
         {
             type: 'input',
             name: 'projectRequirements',
             message: 'Please describe what is required to run the project, eg packages',
-            default: 'No additional packages needed'
         },
         {
             type: 'input',
             name: 'projectUsage',
             message: 'What does the user need to know about running the project?',
-            default: 'Run from terminal using nodejs'
         },
         {
             type: 'list',
@@ -47,23 +43,31 @@ const questions = [
             type: 'input',
             name: 'projectUsername',
             message: 'What is your github username?',
-            default: 'emhat1'
         },
         {
             type: 'input',
             name: 'projectEmail',
             message: 'What is your preferred email address?',
-            default: 'e.hatherell@gmail.com'
-        },
       ])
 
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    console.log(fileName);
+    console.log(data);
+    var data = generateMD(data);
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.error(err) : console.log('You have created a new README.md file')
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) =>
+    writeToFile("README.md", generateMarkdown(answers))
+  );
+}
 
 // Function call to initialize app
 init();
